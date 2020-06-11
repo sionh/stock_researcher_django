104cf5e877c8c11a98e79120b3067883cc5c7a9a
haru <shun_h@hotmail.com>
2020-06-11 18:08:17 +0900

    配当が正常表示できなかった問題を修正 一覧からの検索URLを変更

--- shikihos/urls.py
+++ shikihos/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.IndexView.as_view(), name='index'),
+    path('search', views.IndexView.as_view(), name='index'),
     path('<int:pk>/', views.detail, name='detail'),
     path('code/<int:code>/', views.CodeView.as_view(), name='code'),
 ]
